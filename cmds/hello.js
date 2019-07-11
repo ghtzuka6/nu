60ae304800df9c1a85cb4f29e581a0d1fb6d9ccf
Oliver Boudet <oboudet1@me.com>
2019-07-11 22:41:40 +0200

    fix indetation and formatting. add prettier config as well as .gitignore

--- cmds/hello.js
+++ cmds/hello.js
@@ -1,16 +1,14 @@
 // Generic command template.
 
 module.exports.run = async (bot, message, args) => {
+    // Creates a function called "hello", sends a message, and logs the same thing.
+    function hello() {
+        message.channel.send("Hello world!");
+        console.log("Hello world!");
+    }
 
-// Creates a function called "hello", sends a message, and logs the same thing.
-function hello() {
-    message.channel.send("Hello world!")
-    console.log("Hello world!")
-}
-
-hello();
-
-}
+    hello();
+};
 
 // This is the generic configuration for every command.
 
@@ -20,4 +18,4 @@ module.exports.config = {
     usage: "r/hello",
     description: "Say hello to your working GitHub fork of Ritsu",
     usableby: "@everyone"
-}
+};
